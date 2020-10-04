5e386232b64e721785b6b8544f704ba9f9df0db5
Janessa Garrow <janessa.garrow@gmail.com>
2020-10-03 22:34:28 -0600

    Create footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,10 +2,17 @@
 import { jsx } from "theme-ui"
 import { Fragment } from "react"
 
+import Footer from "../components/Footer"
+
 import "../styles.css"
 
 const Layout = ({ children }) => {
-  return <Fragment>{children}</Fragment>
+  return (
+    <Fragment>
+      <main sx={{ marginBottom: "calc(2rem + 60px)" }}>{children}</main>
+      <Footer />
+    </Fragment>
+  )
 }
 
 export default Layout
