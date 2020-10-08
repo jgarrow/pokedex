bd84ef93e49b906758139478cbb94c5f6eac9591
Janessa Garrow <janessa.garrow@gmail.com>
2020-10-08 10:26:18 -0600

    Create abilities page

---
+++ src/pages/abilities.js
@@ -0,0 +1,16 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui"
+
+import Layout from "../components/Layout"
+import Header from "../components/MainHeader"
+
+const TypeCharts = () => {
+  return (
+    <Layout>
+      <Header showArrow={true} heading="Abilities" />
+      <p sx={{ textAlign: "center" }}>Page under construction</p>
+    </Layout>
+  )
+}
+
+export default TypeCharts
