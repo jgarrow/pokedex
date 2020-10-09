bdaec3959bde86432a6efabc0b05b1df8228c679
Janessa Garrow <janessa.garrow@gmail.com>
2020-10-08 23:04:59 -0600

    Styling tweaks

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,7 +9,15 @@ import "../styles.css"
 const Layout = ({ children }) => {
   return (
     <Fragment>
-      <main sx={{ marginBottom: "calc(2rem + 60px)" }}>{children}</main>
+      <main
+        sx={{
+          marginBottom: "calc(2rem + 60px)",
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        {children}
+      </main>
       <BottomNav />
     </Fragment>
   )
